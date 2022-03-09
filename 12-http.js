57fe3022ebfef7e0bca0cb60a1925534449be9d4
Winston Shaji Jacob <winstonshajijacob@gmail.com>
2022-03-09 14:13:33 -0500

    basic http request

---
+++ 12-http.js
@@ -0,0 +1,18 @@
+const http = require('http')
+
+const server = http.createServer( (req, res) =>  {
+    if (req.url ==='/'){
+        res.end('Welcome to our home page')
+    }
+    else if (req.url ==='/about'){
+        res.end('Here is our short history')
+    }
+    else {
+        res.end(`
+    <h1>Oops!</h1>
+    <p>WE can't seem to find the page you are looking for</p>
+    <a href="/">Back Home</a>
+    `)
+    }
+})
+server.listen(5000)
